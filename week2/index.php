0cf714b00875cc434b78e699e50ff6729f9e690f
fresh-joey <johserf30@gmail.com>
2023-03-15 02:25:59 +0100

    voters validation

--- week2/index.php
+++ week2/index.php
@@ -1,32 +1,20 @@
 <?php
-    // Using a value of age to be 21
-    $age = 17;
-    $pvc = "true";
+    // Using values to make all conditions to be true
+    $age = 18;
+    $pvc = true;
     $ward = 020;
 
-    // if ($age >= 18) {
-    //     echo "Voter is eligible to vote";
-    // }else {
-    //     echo "Voter is below 18, therefore cannot vote";
-    // }
+    if ($age < 18) {
+        echo "Voter is below 18.";
 
+    }elseif ($pvc != "true") {
+        echo "The voter doesn't have PVC.";
 
-    // if ($pvc == "true") {
-    //     echo "The voter has PVC";
-    // }else {
-    //     echo "The voter doesn't have PVC";
-    // }
+    }elseif ($ward != 020) {
+        echo "This voter does not belong to this ward.";
 
-
-    // if ($ward == 020) {
-    //     echo "The voter belongs to this ward, therefore can vote";
-    // } else {
-    //     echo "This voter does not belong to this ward, and cannot be allowed to vote here";
-    // }
-
-
-    if ($age >= 18 && $pvc == "true" && $ward == 020) {
-        echo "Voter is eligible to vote";
     }else {
-        echo "Voter is <b>NOT</b> eligible to vote";
+        echo "Voter is <b>ELIGIBLE</b> to vote";
     }
+
+?>
